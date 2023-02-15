fa413ef38a4855bfed7a9dec27ab3d955ef5e4e7
Fernando Pina dos Santos <fernandopinadossantos@gmail.com>
2023-02-15 17:42:22 +0000

    Fixed debounce

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ async function watchRelativeDepsWithNext() {
                     console.log("\x1b[33m[relative-deps]\x1b[0m Change detected but ignored")
                 } else {
                     running = true
-                    installRelativeDepsWithNext();
+                    debounce(installRelativeDepsWithNext, 100)();
                     running = false
                 }
             })
